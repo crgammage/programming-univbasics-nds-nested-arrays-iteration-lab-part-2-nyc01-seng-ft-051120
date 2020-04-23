ccd70010c1e145ac27cbe01e164ab276b45389ec
Christa Gammage <crgammage@gmail.com>
2020-04-23 20:38:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index] < src[row_index][element_index][0]
+      if src[row_index][element_index] < smallest_numbers[0]
         smallest_numbers << src[row_index][element_index]
     end
     element_index += 1
